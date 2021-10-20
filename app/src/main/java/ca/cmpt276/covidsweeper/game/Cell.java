eb2db9c4fa8a76727c60fe7bddc6276c93aa31a5
harjapdhaliwal <hda36@sfu.ca>
2021-10-19 21:56:53 -0700

    Added working grid

--- app/src/main/java/ca/cmpt276/covidsweeper/game/Cell.java
+++ app/src/main/java/ca/cmpt276/covidsweeper/game/Cell.java
@@ -1,4 +1,29 @@
 package ca.cmpt276.covidsweeper.game;
 
 public class Cell {
+    private boolean clicked = false;
+    private boolean mine = false;
+    public int xCord = -1;
+    public int yCord = -1;
+
+    public Cell(int x, int y) {
+        this.xCord = x;
+        this.yCord = y;
+    }
+
+    public boolean isClicked() {
+        return clicked;
+    }
+
+    public void setClicked(boolean clicked) {
+        this.clicked = clicked;
+    }
+
+    public boolean isMine() {
+        return mine;
+    }
+
+    public void setMine(boolean mine) {
+        this.mine = mine;
+    }
 }
